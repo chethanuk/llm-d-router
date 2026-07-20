3a4cf148d220fb38e531b8a80abcd96509f4cf23
ChethanUK <chethanuk@outlook.com>
2026-07-20 22:39:09 +0200

    test: move GAIE CRD builders into test/framework/gaie
    This completes the wrappers.go removal #1188 asks for. The GAIE builders are
    framework-generic: they touch the GAIE and apix API types and pkg/common/routing,
    never pkg/epp, so they belong beside the other shared test builders rather than
    under pkg/epp/util. With the last symbols gone, the package, its directory and
    its importas alias go too, and no config references the dead path afterwards.
    
    Part of #1188
    
    Signed-off-by: ChethanUK <chethanuk@outlook.com>

--- test/framework/gaie/builders.go
+++ test/framework/gaie/builders.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 The Kubernetes Authors.
+Copyright 2026 The llm-d Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package testing
+// Package gaie provides builders for GAIE and apix inference CRDs used in tests.
+// It imports GAIE/apix API types and pkg/common/routing only, never pkg/epp.
+package gaie
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
